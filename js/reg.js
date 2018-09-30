ff67df0f6915a65689b795c2158dde8a9fc4e10a
SinghKi <singhjagjit873@gmail.com>
2018-09-30 12:49:10 +0800

    jaja

---
+++ js/reg.js
@@ -0,0 +1,45 @@
+let url = 'http://gordoncollegeccs-ssite.net/macionmart/coc/pracapi'
+function x(val){
+	return document.querySelector(val);
+}
+
+document.addEventListener('DOMContentLoaded',function(){
+	x('#reg_user').addEventListener('submit',function(e){
+		let code = Math.floor((Math.random() * 3000) + 10000);
+		let data = {
+			fldUsername:document.getElementById("user_username").value,		
+			fldPassword:document.getElementById("user_password").value,
+			fldFirstname:document.getElementById("user_firstname").value,
+			fldLastname:document.getElementById("user_lastname").value,
+			fldUserType:'laborer',
+			fldEmail:document.getElementById("user_email").value,
+			fldContactNo:document.getElementById("user_phone").value,
+			fldConfirmation:code
+		}
+		let data2 = 'message=Successfully Registered you Confirmation Number is '+code+'&number='+document.getElementById('user_phone').value;
+		e.preventDefault();
+		fetch(url+"/insert/tbl_users",{
+			method:"POST",
+			body:JSON.stringify([data])
+		}).then(function(data){
+			fetch('https://sms-test.bayadcenter.net/sms/push',{
+				method:"POST",
+				body:data2,
+				headers: {
+		    		'Content-Type': 'application/x-www-form-urlencoded'
+		  		}
+			}).then(function(data){
+				if (data['status'] === 200) {
+					console.log('Successfully Sent.');
+				}else{
+					console.log('Error');
+				}
+			});
+		});
+	});
+});
+	
+
+
+
+		
